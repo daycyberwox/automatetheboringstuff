1b437e9cf9434abd840e20caaf7935415bbf3d93
unknown <daysprings2020@gmail.com>
2022-05-30 12:07:08 -0500

    Add Error Handling

--- Error Handling/try_except_cats.py
+++ Error Handling/try_except_cats.py
@@ -8,4 +8,5 @@ try:
     else:
         print("That's not that many cats.")
 except ValueError:
-    print("You did not enter a number.")
+    print("""You did not enter a number. 
+             Please enter a number""")
