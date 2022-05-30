080862531e4a4eb52a779dc13dbb72643b5759a7
unknown <daysprings2020@gmail.com>
2022-05-30 14:12:15 -0500

    Add Lists

---
+++ Lists/negative_indexing_with_lists.py
@@ -0,0 +1,9 @@
+# Negative Indexing
+
+listOne = ["cat", "rat", "dog", "elephant"]
+listThree = ["cat", "rat", "dog", "elephant"]
+print(listThree[-1])        # Prints the last item in the list
+print(listThree[-2])        # Prints the second to the last item in the list
+
+
+print("The " + listThree[-1] + " is afraid of the " + listOne[1] )
